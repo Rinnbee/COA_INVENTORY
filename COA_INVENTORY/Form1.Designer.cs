1f022f0768fe20a85480a0878448ebb577727410
Kimchinana <checherejoy14@gmail.com>
2024-04-08 09:12:18 +0800

    Login Updated

--- COA_INVENTORY/Form1.Designer.cs
+++ COA_INVENTORY/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.gunaPictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gunaPictureBox1.Location = new System.Drawing.Point(0, 0);
             this.gunaPictureBox1.Name = "gunaPictureBox1";
-            this.gunaPictureBox1.Size = new System.Drawing.Size(1540, 845);
+            this.gunaPictureBox1.Size = new System.Drawing.Size(1904, 1041);
             this.gunaPictureBox1.TabIndex = 0;
             this.gunaPictureBox1.TabStop = false;
             // 
@@ -55,11 +55,11 @@
             // 
             this.gunaLabel1.AutoSize = true;
             this.gunaLabel1.BackColor = System.Drawing.Color.White;
-            this.gunaLabel1.Font = new System.Drawing.Font("Bahnschrift", 25F);
+            this.gunaLabel1.Font = new System.Drawing.Font("Bahnschrift", 50F);
             this.gunaLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(170)))), ((int)(((byte)(82)))));
-            this.gunaLabel1.Location = new System.Drawing.Point(1295, 285);
+            this.gunaLabel1.Location = new System.Drawing.Point(1457, 235);
             this.gunaLabel1.Name = "gunaLabel1";
-            this.gunaLabel1.Size = new System.Drawing.Size(102, 41);
+            this.gunaLabel1.Size = new System.Drawing.Size(200, 81);
             this.gunaLabel1.TabIndex = 1;
             this.gunaLabel1.Text = "Login";
             // 
@@ -68,18 +68,17 @@
             this.gunaTextBox1.BackColor = System.Drawing.Color.Transparent;
             this.gunaTextBox1.BaseColor = System.Drawing.Color.White;
             this.gunaTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(170)))), ((int)(((byte)(82)))));
-            this.gunaTextBox1.BorderSize = 1;
             this.gunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.gunaTextBox1.FocusedBaseColor = System.Drawing.Color.White;
             this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
             this.gunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
             this.gunaTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaTextBox1.Location = new System.Drawing.Point(1183, 392);
+            this.gunaTextBox1.Location = new System.Drawing.Point(1354, 441);
             this.gunaTextBox1.Name = "gunaTextBox1";
             this.gunaTextBox1.PasswordChar = '\0';
             this.gunaTextBox1.Radius = 15;
             this.gunaTextBox1.SelectedText = "";
-            this.gunaTextBox1.Size = new System.Drawing.Size(320, 30);
+            this.gunaTextBox1.Size = new System.Drawing.Size(392, 44);
             this.gunaTextBox1.TabIndex = 2;
             // 
             // gunaTextBox2
@@ -87,37 +86,36 @@
             this.gunaTextBox2.BackColor = System.Drawing.Color.Transparent;
             this.gunaTextBox2.BaseColor = System.Drawing.Color.White;
             this.gunaTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(170)))), ((int)(((byte)(82)))));
-            this.gunaTextBox2.BorderSize = 1;
             this.gunaTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.gunaTextBox2.FocusedBaseColor = System.Drawing.Color.White;
             this.gunaTextBox2.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
             this.gunaTextBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText;
             this.gunaTextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaTextBox2.Location = new System.Drawing.Point(1183, 469);
+            this.gunaTextBox2.Location = new System.Drawing.Point(1354, 550);
             this.gunaTextBox2.Name = "gunaTextBox2";
             this.gunaTextBox2.PasswordChar = '\0';
             this.gunaTextBox2.Radius = 15;
             this.gunaTextBox2.SelectedText = "";
-            this.gunaTextBox2.Size = new System.Drawing.Size(320, 30);
+            this.gunaTextBox2.Size = new System.Drawing.Size(392, 44);
             this.gunaTextBox2.TabIndex = 3;
             // 
             // gunaLabel2
             // 
             this.gunaLabel2.AutoSize = true;
-            this.gunaLabel2.Font = new System.Drawing.Font("Bahnschrift", 15F);
-            this.gunaLabel2.Location = new System.Drawing.Point(1194, 365);
+            this.gunaLabel2.Font = new System.Drawing.Font("Bahnschrift", 20F);
+            this.gunaLabel2.Location = new System.Drawing.Point(1362, 405);
             this.gunaLabel2.Name = "gunaLabel2";
-            this.gunaLabel2.Size = new System.Drawing.Size(103, 24);
+            this.gunaLabel2.Size = new System.Drawing.Size(139, 33);
             this.gunaLabel2.TabIndex = 4;
             this.gunaLabel2.Text = "Username";
             // 
             // gunaLabel3
             // 
             this.gunaLabel3.AutoSize = true;
-            this.gunaLabel3.Font = new System.Drawing.Font("Bahnschrift", 15F);
-            this.gunaLabel3.Location = new System.Drawing.Point(1194, 442);
+            this.gunaLabel3.Font = new System.Drawing.Font("Bahnschrift", 20F);
+            this.gunaLabel3.Location = new System.Drawing.Point(1365, 514);
             this.gunaLabel3.Name = "gunaLabel3";
-            this.gunaLabel3.Size = new System.Drawing.Size(99, 24);
+            this.gunaLabel3.Size = new System.Drawing.Size(136, 33);
             this.gunaLabel3.TabIndex = 5;
             this.gunaLabel3.Text = "Password";
             // 
@@ -135,12 +133,12 @@
             this.gunaAdvenceButton1.CheckedLineColor = System.Drawing.Color.DimGray;
             this.gunaAdvenceButton1.DialogResult = System.Windows.Forms.DialogResult.None;
             this.gunaAdvenceButton1.FocusedColor = System.Drawing.Color.Empty;
-            this.gunaAdvenceButton1.Font = new System.Drawing.Font("Bahnschrift", 15F);
+            this.gunaAdvenceButton1.Font = new System.Drawing.Font("Bahnschrift", 20F);
             this.gunaAdvenceButton1.ForeColor = System.Drawing.Color.White;
             this.gunaAdvenceButton1.Image = null;
             this.gunaAdvenceButton1.ImageSize = new System.Drawing.Size(20, 20);
             this.gunaAdvenceButton1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(58)))), ((int)(((byte)(170)))));
-            this.gunaAdvenceButton1.Location = new System.Drawing.Point(1257, 535);
+            this.gunaAdvenceButton1.Location = new System.Drawing.Point(1459, 665);
             this.gunaAdvenceButton1.Name = "gunaAdvenceButton1";
             this.gunaAdvenceButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
             this.gunaAdvenceButton1.OnHoverBorderColor = System.Drawing.Color.Black;
@@ -149,7 +147,7 @@
             this.gunaAdvenceButton1.OnHoverLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(58)))), ((int)(((byte)(170)))));
             this.gunaAdvenceButton1.OnPressedColor = System.Drawing.Color.Black;
             this.gunaAdvenceButton1.Radius = 20;
-            this.gunaAdvenceButton1.Size = new System.Drawing.Size(189, 48);
+            this.gunaAdvenceButton1.Size = new System.Drawing.Size(189, 59);
             this.gunaAdvenceButton1.TabIndex = 6;
             this.gunaAdvenceButton1.Text = "Login";
             this.gunaAdvenceButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -159,7 +157,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1540, 845);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
             this.Controls.Add(this.gunaAdvenceButton1);
             this.Controls.Add(this.gunaLabel3);
             this.Controls.Add(this.gunaLabel2);
